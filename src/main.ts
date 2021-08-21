a9d6c5a60b4818826a45969e3f4e3b27d612c368
Tyler Schloesser <tschloes@amazon.com>
2021-08-21 07:56:32 -0700

    handle null by

--- src/main.ts
+++ src/main.ts
@@ -119,6 +119,13 @@ interface HnUpdates {
           title: null,
           kids: null,
         }, itemSnap.val())
+
+        // TODO figure out why this is null?
+        if (item.by === null) {
+          console.log(`item.by is null? skipping`, item)
+          return
+        }
+
         try {
           await Item.upsert(item)
         } catch (error) {
